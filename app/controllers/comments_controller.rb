f5acb8c3a3ffbf8385a7b25294c2386bf13ddf18
Jack9145 <toikakunie@gmail.com>
2020-12-29 19:15:50 +0900

    非同期でのコメント投稿機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,17 @@
 class CommentsController < ApplicationController
   def create 
-    Comment.create(comment_params)
-    redirect_to post_path
+   
+    @comment = Comment.create(comment_params)
+    find_post
+    @comments = @post.comments.includes(:user)
   end
 
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
+
+  def find_post
+    @post = @comment.post
+  end 
 end
