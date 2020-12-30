0689a2473404b95777bc0c7250a90e225db3718e
Jack9145 <toikakunie@gmail.com>
2020-12-30 11:34:41 +0900

    単体テストコードの実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
+RSpec.describe Post, type: :model do
+  before do
+    @user = FactoryBot.create(:user)
+    @post = FactoryBot.build(:post, user_id: @user.id)
+    @post.image = fixture_file_upload("app/assets/images/muscle.jpg")
+    @post.save
+    @comment = FactoryBot.build(:comment, user_id: @user.id, post_id: @post.id)
+    sleep(0.5)
+  end
+  describe 'コメント雨が投稿できるとき' do
+    it 'ユーザーと投稿が存在していて、所定のフォームでコメントが入力されたとき' do
+      expect(@comment).to be_valid
+    end
+  end
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'コメントが投稿できないとき' do
+    it 'コメントが空のとき' do
+      @comment.text = ""
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Textを入力してください")
+    end
+
+    it 'コメントが150字を超えるとき' do
+      @comment.text = "a"*151
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Textは150字以内で入力してください")
+    end
+
+    it 'ユーザーが存在しないとき' do
+      @comment.user_id = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Userを入力してください")
+    end
+
+    it 'コメントする投稿が存在しないとき' do
+      @comment.post_id = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Postを入力してください")
+    end
+  end
 end
