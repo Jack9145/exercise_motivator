eed4ed0dd8548087bac050488e75dc59da6f3222
Jack9145 <toikakunie@gmail.com>
2020-12-30 11:34:22 +0900

    エラーメッセージの修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :text,  presence: true,length: {maximum: 150}
+  validates :text,  presence: true,length: {maximum: 150, message:"は150字以内で入力してください"}
 end
