dc4c43b2a21fd7917f916657a44ae7851a3c0121
Jack9145 <toikakunie@gmail.com>
2021-01-06 16:52:20 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments
+  has_many :favorites,dependent: :destroy
   with_options presence: true do
     validates :nickname
     validates :email
     validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字を含む６文字以上で設定してください' }
+  end
+  def already_favorited?(post)
+    self.favorites.exists?(post_id: post.id)
   end
 end
  
