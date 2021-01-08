fd928c04a3340257532db20541c7150db0b06b51
Jack9145 <toikakunie@gmail.com>
2021-01-08 19:30:20 +0900

    プロフィールモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_063931) do
+ActiveRecord::Schema.define(version: 2021_01_08_022109) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,16 @@ ActiveRecord::Schema.define(version: 2021_01_06_063931) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.decimal "height", precision: 4, scale: 1, null: false
+    t.decimal "weight", precision: 4, scale: 1, null: false
+    t.decimal "fat_rate", precision: 3, scale: 1
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -72,4 +82,5 @@ ActiveRecord::Schema.define(version: 2021_01_06_063931) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "posts", "users"
+  add_foreign_key "profiles", "users"
 end
