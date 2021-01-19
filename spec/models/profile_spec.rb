6cb362dc362a32643d517084053928d609fccd56
Jack9145 <toikakunie@gmail.com>
2021-01-19 12:02:03 +0900

    単体テストコードの実装

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @profile = FactoryBot.build(:profile)
+  end
+  describe "プロフィールが登録できる時" do
+    it "全ての項目が所定のフォームで入力された時" do
+      expect(@profile).to be_valid
+    end
+  end
+
+  describe "プロフィール登録ができないとき" do
+    it "身長が入力されていない時" do
+      @profile.height = ""
+      @profile.valid?
+      expect(@profile.errors.full_messages).to include("身長を入力してください")
+    end
+
+    it "身長が4桁以上の数字で入力されている時" do
+      @profile.height = "1000"
+      @profile.valid?
+      expect(@profile.errors.full_messages).to include("身長は不正な値です")
+    end
+    
+    it "体重が入力されていない時" do
+      @profile.weight = ""
+      @profile.valid?
+      expect(@profile.errors.full_messages).to include("体重を入力してください")
+    end
+
+    it "体重が4桁以上で入力されている時" do
+      @profile.weight = 1000
+      @profile.valid?
+      expect(@profile.errors.full_messages).to include("体重は不正な値です")
+    end
+  end
 end
